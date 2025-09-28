403ece2e02b646d8447f97f1d88ae893324e0b83
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-28 22:52:51 +0300

    Update fxmanifest.lua

--- tpz_inventory/fxmanifest.lua
+++ tpz_inventory/fxmanifest.lua
@@ -4,12 +4,12 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE - Inventory'
-version '1.0.3'
+version '1.0.1'
 
 ui_page 'html/index.html'
 
 shared_scripts { 'config.lua', 'locales.lua', 'item_registrations.lua', 'weapon_registrations.lua' }
-client_scripts { '@tpz_core/client/tp-client_dataview.lua', 'client/*.lua' }
+client_scripts { '@tpz_core/client/modules/dataview/client.lua', 'client/*.lua' }
 server_scripts { 'server/*.lua' }
 
 files { 'html/**/*' }
@@ -18,5 +18,3 @@ dependency { 'tpz_core' }
 
 
 lua54 'yes'
-
-
