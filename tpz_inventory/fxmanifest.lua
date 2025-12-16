d366179f5cd221328c97dafd1630662c35601eb1
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-12-16 02:03:20 +0200

    Update fxmanifest.lua

--- tpz_inventory/fxmanifest.lua
+++ tpz_inventory/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE - Inventory'
-version '1.1.0'
+version '1.1.1'
 
 ui_page 'html/index.html'
 
@@ -18,6 +18,7 @@ dependency { 'tpz_core' }
 
 
 lua54 'yes'
+
 
 
 
